11541da45c5ce12ac930ee5cc0559198240181c8
semaHbo <semaahacibekiroglu@gmail.com>
2025-04-28 10:52:36 +0300

    CORS hatasi giderildi

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -1,28 +1,17 @@
+import pymysql
+pymysql.install_as_MySQLdb()
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_mail import Mail
 from flask_pymongo import PyMongo
+from flask_cors import CORS  # CORS'u import et
+
 
 db = SQLAlchemy()
 jwt = JWTManager()
 mail = Mail()
-
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object('config.Config')
-
-    db.init_app(app)
-    jwt.init_app(app)
-    mail.init_app(app)
-
-    # Blueprint'ler sonra burada eklenecek
-        # Blueprint'leri burada ekleyeceğiz
-    from app.routes.auth import auth_bp
-    app.register_blueprint(auth_bp)
-
-    return app
-
 mongo = PyMongo()
 
 def create_app():
@@ -32,10 +21,12 @@ def create_app():
     db.init_app(app)
     jwt.init_app(app)
     mail.init_app(app)
-    mongo.init_app(app)  # 🔥 MongoDB bağlantısı
+    mongo.init_app(app)
 
+    CORS(app)  # 🔥 CORS'u burada aktif ettik
+
+    # Blueprint'leri burada ekliyoruz
     from app.routes.auth import auth_bp
     app.register_blueprint(auth_bp)
 
     return app
-
